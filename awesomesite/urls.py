e498d08d0c219ec9aace5e14315059d663e61937
dicksondurosakin <durosakindickson@gmail.com>
2022-01-18 10:25:58 +0100

    finally got static to work

--- awesomesite/urls.py
+++ awesomesite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from django.conf.urls.static import static
+from django.conf import settings
 
 sitemaps = {
  'posts': PostSitemap,
@@ -27,5 +29,4 @@ urlpatterns = [
     path('blog/',include('blog.urls',namespace='blog')),
     path('',include('portfolio.urls',namespace='portfolio')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap')
-
 ]
