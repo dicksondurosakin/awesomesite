34cf9425a470e2c18ec2758656d535ed16c1186a
dicksondurosakin <durosakindickson@gmail.com>
2022-01-21 22:51:15 +0100

    redirect corrected

--- account/views.py
+++ account/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
 
-
 from account.forms import UserRegistrationForm
 
 
@@ -16,25 +15,26 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,username=cd['username'].lower(),password=cd['password'])
+            user = authenticate(request, username=cd['username'].lower(), password=cd['password'])
             if user is not None:
                 if user.is_active:
-                    login(request,user)
-                    return redirect('dashboard')
+                    login(request, user)
+                    return redirect('account:dashboard')
                 else:
                     return HttpResponse('Disabled account')
             else:
                 messages.error(request, "Username or password Incorrect")
     else:
         form = LoginForm()
-    return render(request,'account/login.html',{'form':form})
+    return render(request, 'account/login.html', {'form': form})
 
 
 @login_required
 def dashboard(request):
- return render(request,
- 'account/dashboard.html',
- {'section': 'dashboard'})
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
+
 
 def register(request):
     if request.method == 'POST':
@@ -43,33 +43,31 @@ def register(request):
             # create a new user but avoids saving it yet
             # create  the object but don't commit yet 
             new_user = user_form.save(commit=False)
-            # set the password but you have to hash it unless it django wont save it
+            # set the password, but you have to hash it unless it django wont save it
             new_user.set_password(user_form.cleaned_data['password2'])
             new_user.username = user_form.cleaned_data['username'].lower()
             # finally save it
             new_user.save()
             Profile.objects.create(user=new_user)
-            return render(request, 'account/register_done.html',{'new_user':new_user})
+            return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request,'account/register.html',{'user_form':user_form})
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 
 @login_required
 def edit(request):
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,
-                                        files=request.FILES)
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST,
+                                       files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request,"Profile Updated Successfully")
+            messages.success(request, "Profile Updated Successfully")
         else:
             messages.error(request, "Error Updating Profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(request,'account/edit.html',{'user_form':user_form,'profile_form':profile_form})
-
-
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
