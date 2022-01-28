483217f5214338ebda3fd14f8f3fe1ea3974de83
dicksondurosakin <durosakindickson@gmail.com>
2022-01-29 00:40:03 +0100

    edited bookmark

--- account/templates/bookmarklet_ launcher.js
+++ account/templates/bookmarklet_ launcher.js
@@ -4,7 +4,7 @@
     }
     else {
     document.body.appendChild(document.createElement('script')).
-   src='https://127.0.0.1:8000/account/static/js/bookmarklet.js?r='+Math.
+   src='https://www.dicksondurosakin.com/account/static/js/bookmarklet.js?r='+Math.
    floor(Math.random()*99999999999999999999);
     }
    })();
