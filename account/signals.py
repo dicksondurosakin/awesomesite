da4e3f89ef0353bd2f001baa8b9bf24c9adf20d1
dicksondurosakin <durosakindickson@gmail.com>
2022-02-14 00:24:11 +0100

    signals done

---
+++ account/signals.py
@@ -0,0 +1,12 @@
+from turtle import pos
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Profile
+from django.contrib.auth.models import User
+
+
+@receiver(post_save,sender=User)
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        new_profile = Profile.objects.create(user=instance)
+        new_profile.save()
